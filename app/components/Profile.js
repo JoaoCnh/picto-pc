011a2c7fdc39a3540467a33f10b29d0885a1a716
JoaoCnh <joaomarinho.cunha@gmail.com>
2016-12-12 12:35:07 +0000

    [app] Profile and NavMenu doesn't sit on top of Login and Register

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
 
+import AuthBubbles from './auth/AuthBubbles';
+
+import genStyles from './Home.css';
+import styles from './Profile.css';
+
+import AuthAPI from '../api/auth';
+
 export default class Profile extends Component {
+    constructor(props) {
+        super(props);
+
+        this.currentUser = AuthAPI.getLogin();
+    }
     render() {
         return (
-            <div>
-                <h1>Profile</h1>
+            <div className={genStyles.bubbleWrapper}>
+
+                <div className={styles.profileCard}>
+                    <div className={styles.profile}>
+                        <img src="../resources/usericons/default.png" className={styles.border} />
+                    </div>
+                    <div className={styles.profileName}>
+                        {this.currentUser.username}
+                    </div>
+                </div>
+
+                <AuthBubbles />
             </div>
         );
     }
--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,6 @@
 // @flow
 import React from 'react';
-import { Route, IndexRoute } from 'react-router';
+import { Router, Route, IndexRoute } from 'react-router';
 import AppContainer from './containers/AppContainer';
 import HomePage from './containers/HomePage';
 import ProfilePage from './containers/ProfilePage';
@@ -8,12 +8,13 @@ import LoginPage from './containers/LoginPage';
 import RegisterPage from './containers/RegisterPage';
 
 export default (
-    <Route path="/" component={AppContainer}>
-        <IndexRoute component={HomePage} />
+    <Router>
+        <Route path="/" component={AppContainer}>
+            <IndexRoute component={HomePage} />
 
-        <Route path="profile" component={ProfilePage} />
-
-        <Route path="auth/login" component={LoginPage} />
-        <Route path="auth/register" component={RegisterPage} />
-    </Route>
+            <Route path="profile" component={ProfilePage} />
+        </Route>
+        <Route path="/auth/login" component={LoginPage} />
+        <Route path="/auth/register" component={RegisterPage} />
+    </Router>
 );
