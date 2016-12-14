ef4778b12aa7e196e3c4b5086cf22457c492ac48
JoaoCnh <joaomarinho.cunha@gmail.com>
2016-12-14 11:39:34 +0000

    [app] App overall container is now covered in BubbleWrap and bubbles are actual Bubbles

--- app/components/App.js
+++ app/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Nav from './layout/Nav';
+import BubbleWrap from './common/BubbleWrap';
 
 import AuthAPI from '../api/auth';
 
@@ -18,14 +19,14 @@ export default class App extends Component {
 
     render() {
         return (
-            <div>
+            <BubbleWrap>
                 <Nav route={this.props.location.pathname}
                     active={this.props.app.isMenuActive}
                     toggleMenu={this.props.toggleMenu}
                     logout={this._logout.bind(this)} />
 
                 {this.props.children}
-            </div>
+            </BubbleWrap>
         );
     }
 }
